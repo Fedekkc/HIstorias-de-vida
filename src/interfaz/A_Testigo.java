712370ec7519619ba648f20527ae5304f6c6afca
aylviss <isasiaylen@gmail.com>
2023-10-31 22:01:55 -0300

    WADUSHUMIN

--- src/interfaz/A_Testigo.java
+++ src/interfaz/A_Testigo.java
@@ -12,11 +12,16 @@ import java.awt.Panel;
 import java.awt.List;
 import java.awt.Color;
 import com.toedter.calendar.JDateChooser;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JTextPane;
 
 public class A_Testigo extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
+	private JTextField Nombre;
+	private JTextField DNI;
 
 	/**
 	 * Launch the application.
@@ -49,6 +54,36 @@ public class A_Testigo extends JDialog {
 		panel.setBackground(new Color(50, 49, 78));
 		contentPanel.add(panel);
 		panel.setLayout(null);
+		
+		Nombre = new JTextField();
+		Nombre.setForeground(new Color(108, 108, 108));
+		Nombre.setHorizontalAlignment(SwingConstants.CENTER);
+		Nombre.setText("Nombre");
+		Nombre.setBounds(135, 41, 102, 30);
+		panel.add(Nombre);
+		Nombre.setColumns(10);
+		
+		DNI = new JTextField();
+		DNI.setForeground(new Color(108, 108, 108));
+		DNI.setHorizontalAlignment(SwingConstants.CENTER);
+		DNI.setText("DNI");
+		DNI.setBounds(135, 82, 102, 30);
+		panel.add(DNI);
+		DNI.setColumns(10);
+		
+		JButton botonAceptar = new JButton("Aceptar");
+		botonAceptar.setBounds(195, 195, 89, 23);
+		panel.add(botonAceptar);
+		
+		JButton botonCancelar = new JButton("Cancelar");
+		botonCancelar.setBounds(78, 195, 89, 23);
+		panel.add(botonCancelar);
+		
+		JTextPane txtpnTestimonio = new JTextPane();
+		txtpnTestimonio.setForeground(new Color(108, 108, 108));
+		txtpnTestimonio.setText("Testimonio");
+		txtpnTestimonio.setBounds(119, 123, 139, 48);
+		panel.add(txtpnTestimonio);
 		
 		List list = new List();
 		list.setBounds(10, 10, 165, 379);
